961be19315e31453063207c993a17aca1a730d86
Jim Lin <57472539+Hendario@users.noreply.github.com>
2020-11-14 18:00:38 -0500

    So, I have changed the stuff, base on the menu
    So, I have changed part 1 into the menu option, so when the system is opened, users can choose which data they want to see. .

--- Project 1 Code.py
+++ Project 1 Code.py
@@ -4,10 +4,10 @@ def main():
 
     while True:
         meun()
-        choice = int(input('Plese select'))
+        choice = int(input('Plese select\n'))
         if choice in [1,2,3,]:
             if choice == 1:
-                student_id()
+                student_inform()
                 break
 
             elif choice == 2:
@@ -16,27 +16,32 @@ def main():
             elif choice == 3:
                 change()
                 break
+
+def student_inform():
+    filename = "students.csv"
+
+    with open(filename) as csvfile:
+       reader = csv.DictReader(csvfile)
+       student_id = input('Enter the student id: ')
+       for row in reader:
+           if student_id == row['student_id']:
+               for key,value in row.items():
+                   print(key,':',value)
+               break
+       else:
+           print('This id does not exist')
+
+
 def meun():
     print("----------------meun--------------------")
     print("|         1.Student information        |")
     print("|         2.Courses                    |")
+    print("|         3.Change                     |")
     print("----------------------------------------")
 
-def student_id():
-    with open('students.csv','r') as csvfile:
-        reader = csv.DictReader(csvfile)
-        student_id = input('Enter the student number: ')
-        for row in reader:
-            if student_id == row['student_id']:
-                for key, value in row.items():
-                    print(key, ':', value)
-                break
-
-            else:
-                print('This number does not exist')
 
 def course():
-    with open('courses.csv','r') as gradefile:
+    with open('Student_courses.csv', 'r') as gradefile:
         read = csv.DictReader(gradefile)
         student_id = input('Enter the student number: ')
         for row in read:
